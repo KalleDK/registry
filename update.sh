6cb18d7fe66cb06bfe92acb84560461b3ad6d09e
Kalle M. Aagaard <km@conscia.com>
2024-11-07 11:02:36 +0100

    multi toml

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 find . -name 'fetcher*.toml' -print0 | while IFS= read -r -d '' file; do
     dir=$(realpath $(dirname "$file"))
-    docker run --rm -v $dir:/app fetcher $(basename "$file")
+    docker run --rm -v $dir:/app fetcher fetcher --config $(basename "$file")
 done
